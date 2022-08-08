1231cc21102a3e9d210e9a6f89018aa7fe0efefd
Marlon Lede <marlonlede@thesting.com>
2022-08-08 13:04:30 +0200

    add theme and type

---
+++ src/theme/theme.ts
@@ -0,0 +1,30 @@
+import { Theme } from "@emotion/react";
+
+export const theme: Theme = {
+  colors: {
+    white: "#FFF",
+    black: "#000",
+    main100: "#303030",
+    main200: "#1C3F1D",
+    main300: "#46a08c",
+    accent100: "#736A31",
+    accent200: "#A19247",
+  },
+  spacings: {
+    xs: 4,
+    s: 8,
+    m: 16,
+    l: 24,
+    xl: 32,
+    xxl: 40,
+    xxxl: 48,
+  },
+  typography: {
+    fontS: "14px",
+    fontM: "18px",
+    fontL: "22px",
+    fontXL: "26px",
+    fontXXL: "30px",
+    fontXXXL: "34px",
+  },
+};
