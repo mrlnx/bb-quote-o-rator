6a034392db7233f41daa67314596e64eb895e5a7
Marlon Lede <marlonlede@thesting.com>
2022-08-08 13:00:20 +0200

    add test for useStoredList

---
+++ src/hooks/useStoredList.spec.ts
@@ -0,0 +1,39 @@
+import { act } from "@testing-library/react";
+import { renderHook } from "@testing-library/react-hooks";
+import { useStoredList } from "./useStoredList";
+
+describe("useStoredList", () => {
+  const setup = () => renderHook(() => useStoredList<number>("test-key"));
+
+  it("should show items", async () => {
+    const { result } = setup();
+
+    act(() => {
+      result.current.add(1);
+      result.current.add(3);
+    });
+
+    expect(result.current.items).toContain(1);
+    expect(result.current.items).toContain(3);
+  });
+
+  it("should remove item", () => {
+    const { result } = setup();
+
+    act(() => {
+      result.current.add(1);
+      result.current.add(3);
+    });
+
+    expect(result.current.items).toContain(1);
+    expect(result.current.items).toContain(3);
+
+    act(() => {
+      result.current.add(1);
+      result.current.remove(3);
+    });
+
+    expect(result.current.items).toContain(1);
+    expect(result.current.items).not.toContain(3);
+  });
+});
